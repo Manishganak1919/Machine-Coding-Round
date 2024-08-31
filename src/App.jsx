75b856c2b6d4a13f7457bfdd9111131761781f22
Manish kumar <tejnarayan1919@gmail.com>
2024-08-31 23:14:04 +0530

    Set up react with vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red">
+      <h3 className="text-green-500">Manish Kumar</h3>
+    </div>
+  );
+}
+
+export default App;
